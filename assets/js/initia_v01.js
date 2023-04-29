1a780a425bfa40c733ad4b48b90c0c49dc3e6a26
ksg <ryuzuka1222>
2023-04-29 19:52:57 +0900

    visualText scroll motion

--- assets/js/initia_v01.js
+++ assets/js/initia_v01.js
@@ -19,3 +19,37 @@ textLines.forEach((textLine) => {
   });
   animateText(textLine);
 });
+
+(function () {
+  let scrollY = 0
+  let textTop = []
+  let visualText = Array.from(document.getElementsByClassName('animate')).map((el, index) => {
+    let textEl = el.parentElement
+    textTop[index] = textEl.offsetTop
+    Object.assign(textEl.style, {
+      transition: '.5s ease-out 0s',
+      transform: 'translate3d(0px, 0px, 0px)'
+    })
+    return textEl
+  })
+
+  window.addEventListener('resize', e => {
+    scrollY = e.currentTarget.scrollY
+    textTop = visualText.map(el => el.offsetTop)
+  })
+
+  window.addEventListener('scroll', e => {
+    scrollY = e.currentTarget.scrollY
+    transform()
+  })
+
+  function transform () {
+    visualText.forEach((el, index) => {
+      let outX = (index % 2) ? -1.2 * visualText[index].clientWidth : window.innerWidth
+      let translateX = Math.round(outX / textTop[index] * scrollY)
+
+      el.style.transform = `translate3d(${translateX}px, 0px, 0px)`
+    })
+  }
+
+})()
